f7e97bdaf7bdb1aa84b50600004813cb1a3aa07c
swetha-A130 <swethachintalapudi6@gmail.com>
2024-10-22 23:51:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,46 @@
-//complete this code
-class Person {}
+//complete this co
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  // Getter for name
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  // Getter for age
+  get age() {
+    return this._age;
+  }
 
+  // Setter for age
+  set age(age) {
+    if (typeof age === "number" && age >= 0) {
+      this._age = age;
+    } else {
+      console.error("Please provide a valid age.");
+    }
+  }
+}
+
+class Student extends Person {
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+class Teacher extends Person {
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
+
+// Do not change the code below this line
+window.Person = Person;
+window.Student = Student;
+window.Teacher = Teacher;
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
